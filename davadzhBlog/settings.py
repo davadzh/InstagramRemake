0b84fa2f2b1eb14d9f9e9e49e0ca41c5e592e6bc
davadzh <42493605+davadzh@users.noreply.github.com>
2020-04-27 14:43:38 +0300

    убрал режим дебага

--- davadzhBlog/settings.py
+++ davadzhBlog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!awzy*-pk^rj*#z9_xuuvuz+nlm_4%!^$*^_r8ep#!9oe(x75w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['instonksgram.herokuapp.com', '127.0.0.1']
 
